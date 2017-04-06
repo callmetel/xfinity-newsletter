58e92a1f4a07af4fca8bd90f34edd725daa5c113
Chantel <chantel@Chantels-iMac.local>
2017-04-06 12:10:15 -0400

    Added templated version of site

---
+++ static/template/app/js/lib/function.js
@@ -0,0 +1,262 @@
+$(document).ready(function() {
+
+	// Get IE or Edge browser version
+	    var version = detectIE();
+
+	    if (version === false) {
+	      $('html').removeClass('IE');
+	    } 
+	    else if (version >= 12) {
+	    	 console.log('You are using IE');
+	      $('html').addClass('IE');
+	    } 
+	    else {
+	      console.log('You are using IE');
+	      $('html').addClass('IE');
+	    };
+
+	    function detectIE() {
+	      var ua = window.navigator.userAgent;
+
+	      var msie = ua.indexOf('MSIE ');
+	      if (msie > 0) {
+	        // IE 10 or older => return version number
+	        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+	      }
+
+	      var trident = ua.indexOf('Trident/');
+	      if (trident > 0) {
+	        // IE 11 => return version number
+	        var rv = ua.indexOf('rv:');
+	        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+	      }
+
+	      var edge = ua.indexOf('Edge/');
+	      if (edge > 0) {
+	        // Edge (IE 12+) => return version number
+	        return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+	      }
+
+	      // other browser
+	      return false;
+	    };
+	    var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+
+		var introHeight,headerHeight,introPinDuration,remoteDuration;
+		var $window = $(window);
+		var body = $('html, body');
+
+		TweenLite.to($window, .2, {scrollTo:1});
+
+		var curScroll, article1, article2, article3, article4, article1Title, article2Title, article3Title, article4Title, headingScrollPos;
+
+		$(window).scroll(function(e){
+			curScroll = $(this).scrollTop();
+			article1 = parseInt($('#article1').offset().top) - curScroll;
+			article2 = parseInt($('#article2').offset().top) - curScroll;
+			article3 = parseInt($('#article3').offset().top) - curScroll;
+			article4 = parseInt($('#article4').offset().top) - curScroll;
+			article1Title = parseInt($('#article1-title').offset().top) - curScroll;
+			article2Title = parseInt($('#article2-title').offset().top) - curScroll;
+			article3Title = parseInt($('#article3-title').offset().top) - curScroll;
+			article4Title = parseInt($('#article4-title').offset().top) - curScroll;
+		});
+
+
+	// Create isMobile Function
+
+		var isMobile = {
+            detectMobile: function() {
+                return navigator.userAgent.match(/Mobi/i);
+            }
+        };
+
+        if(isMobile.detectMobile()){
+        	$('#remote-on').attr('src','app/images/mobile-new-remote-on.png');
+        	headingScrollPos = 90;
+        } else{
+        	headingScrollPos = 30;
+
+        }
+
+        $(window).resize(function(){
+        	if($(window).width() > 768){
+        		$('#remote-on').attr('src','app/images/new-remote-on.png');	
+        	} else {
+        		$('#remote-on').attr('src','app/images/mobile-new-remote-on.png');
+        	};
+        	introHeight = $('#intro').height();
+			headerHeight = $('header').height();
+			introPinDuration = Math.round(introHeight/2);
+			remoteDuration = Math.round($('#menu').height()/2);
+
+			$('.scrollmagic-pin-spacer').css('min-height', introHeight);
+        }).resize();
+
+        if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
+		    $('.firefox-clip').addClass('is-active');
+		    $('body').addClass('firefox-in-use');
+		} else{
+			$('.firefox-clip').removeClass('is-active');
+		    $('body').removeClass('firefox-in-use');
+		};
+
+        function printPDF()
+		{
+		  var pdfFrame = window.frames["pdf"];
+		  pdfFrame.focus();
+		  pdfFrame.print();
+		}
+
+		$('.print-pdf').on('click', function(e){
+			e.preventDefault();
+			console.log('clicked');
+			printPDF();
+		});
+
+		// Read More Scroll To Functions
+
+			$('#article1-heading .read-article-btn').on('click', function(e){
+				e.preventDefault();
+				TweenLite.to($window, 1, {scrollTo:curScroll + article1 - 20});
+			});
+			$('#article2-heading .read-article-btn').on('click', function(e){
+				e.preventDefault();
+				TweenLite.to($window, 1, {scrollTo:curScroll + article2 - 20});
+			});
+			$('#article3-heading .read-article-btn').on('click', function(e){
+				e.preventDefault();
+				TweenLite.to($window, 1, {scrollTo:curScroll + article3 - 20});
+			});
+			$('#article4-heading .read-article-btn').on('click', function(e){
+				e.preventDefault();
+				TweenLite.to($window, 1, {scrollTo:curScroll + article4 - 20});
+			});
+
+		$('.back-btn').on('click', function(e){
+			e.preventDefault();
+			body.animate({ scrollTop: 0 }, 2000, 'swing');
+		});
+
+		// Nav Scroll To Functions
+
+			$('.nav-link:eq(0)').on('click', function(e){
+				e.preventDefault();
+				body.animate({ scrollTop: curScroll + article1Title - headingScrollPos}, 2000, 'swing');
+				
+				setTimeout(function(){
+					$('nav').removeClass('is-active');
+				}, 700);
+			});
+			$('.nav-link:eq(1)').on('click', function(e){
+				e.preventDefault();
+				body.animate({ scrollTop: curScroll + article2Title - headingScrollPos}, 2000, 'swing');
+				setTimeout(function(){
+					$('nav').removeClass('is-active');
+				}, 700);
+			});
+			$('.nav-link:eq(2)').on('click', function(e){
+				e.preventDefault();
+				body.animate({ scrollTop: curScroll + article3Title - headingScrollPos}, 2000, 'swing');
+				setTimeout(function(){
+					$('nav').removeClass('is-active');
+				}, 700);
+			});
+			$('.nav-link:eq(3)').on('click', function(e){
+				e.preventDefault();
+				body.animate({ scrollTop: curScroll + article4Title - headingScrollPos}, 2000, 'swing');
+				setTimeout(function(){
+					$('nav').removeClass('is-active');
+				}, 700);
+			});
+
+		// Menu Scroll To Functions
+		
+			$('#menu ul a:eq(0)').on('click', function(e){
+				e.preventDefault();
+				body.animate({ scrollTop: curScroll + article1Title - headingScrollPos}, 2000, 'swing');
+			});
+			$('#menu ul a:eq(1)').on('click', function(e){
+				e.preventDefault();
+				body.animate({ scrollTop: curScroll + article2Title - headingScrollPos}, 2000, 'swing');
+			});
+			$('#menu ul a:eq(2)').on('click', function(e){
+				e.preventDefault();
+				body.animate({ scrollTop: curScroll + article3Title - headingScrollPos}, 2000, 'swing');
+			});
+			$('#menu ul a:eq(3)').on('click', function(e){
+				e.preventDefault();
+				body.animate({ scrollTop: curScroll + article4Title - headingScrollPos}, 2000, 'swing');
+			});
+
+	// Hide Pages from Aria
+		
+		function addAriaHiddenPages(){
+
+		}
+
+	// Tab Index Functions
+	
+		// $("body").on("keydown", function(event) {
+		// 	$('*').removeClass('no-focus');
+	 //    });
+    var scrollTime = 1.2;
+    var scrollDistance = 200;
+
+			
+			//Begin ScrollMagic animation
+			
+			// Initialize ScrollMagic Controller
+			var scrollMagicController = new ScrollMagic.Controller();
+
+			var scene1A = new ScrollMagic.Scene({
+							duration: introPinDuration,
+							offset: 0 /* offset the trigger 150px below #scene's top */
+						})
+						.setPin("#intro-container");
+
+			var tween1B = TweenMax.set('.xd-logo', {className:'xd-logo absolute-center'});
+
+			var scene1B = new ScrollMagic.Scene({
+							duration: 0,
+							offset: 5 /* offset the trigger 150px below #scene's top */
+						})
+						.setTween(tween1B);
+
+			var tween1C = TweenMax.set('#intro .article', {className:'article absolute-center fade-up-in-2', delay:.5});
+
+			var scene1C = new ScrollMagic.Scene({
+							duration: 0,
+							offset: 5 /* offset the trigger 150px below #scene's top */
+						})
+						.setTween(tween1C);
+
+			var tween2A = TweenMax.set('.extra-burger', {className:'+=active'});
+
+			var scene2A = new ScrollMagic.Scene({
+							duration: 0,
+							offset: introPinDuration + 20 /* offset the trigger 150px below #scene's top */
+						})
+						.setTween(tween2A);
+			scrollMagicController.addScene([
+			  scene1A,
+			  scene1B,
+			  scene1C,
+			  scene2A
+			]);
+
+	// Mobile Nav 
+		
+		$('#burger, #burger > img, .extra-burger.active, .extra-burger.active > img').on('click', function(){
+			$('nav').addClass('is-active');
+		});
+
+		$('#close').on('click', function(){
+			$('nav').removeClass('is-active');
+		});
+    
+
+});
+
+
+
--- wp-content/themes/xfinity_newsletter/footer.php
+++
@@ -1,6 +0,0 @@
-<?php wp_footer(); ?>
-<script type='text/javascript' id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3002/browser-sync/browser-sync-client.2.11.1.js'><\/script>".replace("HOST", location.hostname));
-//]]></script>
-</body>
-</html>
